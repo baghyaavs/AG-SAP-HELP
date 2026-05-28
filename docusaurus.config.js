81d83ddb99b35fa74985f8af019ba3392760ddd1
baghyaavs <baghyaavs007@gmail.com>
2026-05-28 16:50:29 +0530

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,9 +10,9 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'AG SAP Help',
+  tagline: 'Welcome to AG SAP Help Portal',
+  favicon: 'img/aglogo.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -83,7 +83,7 @@ const config = {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'AG SAP Help',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
