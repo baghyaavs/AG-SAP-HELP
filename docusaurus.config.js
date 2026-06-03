fd6638eaf7a73fe863b1c40cc5f7347f2b62139a
baghyaavs <baghyaavs007@gmail.com>
2026-06-03 15:15:51 +0530

    updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'baghyaavs', // Usually your GitHub org/user name.
   projectName: 'AG-SAP-HELP', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
